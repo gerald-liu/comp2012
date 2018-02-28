4adeba749229b5d5b33cee2ba72893c79b8c9661
gerald-liu <36258909+gerald-liu@users.noreply.github.com>
2018-02-28 15:56:19 +0800

    update MIL

--- lab2/folder.cpp
+++ lab2/folder.cpp
@@ -4,15 +4,13 @@
 
 using namespace std;
 
-Folder::Folder(unsigned int size): maxSize(size), currSize{0} {
+Folder::Folder(unsigned int size): maxSize{ static_cast<int>(size) }, currSize{ 0 } {
     files = new const File* [currSize];
 }
 
-Folder::~Folder() {
-    delete[] files;	
-}
+Folder::~Folder() { delete[] files;	}
 
-Folder::Folder(const Folder &folder): maxSize(folder.maxSize), currSize{folder.currSize} {
+Folder::Folder(const Folder &folder): maxSize{ static_cast<int>(folder.maxSize) }, currSize{ folder.currSize } {
     files = new const File* [currSize];
     for (int i=0; i<currSize; i++) files[i] = new File{*folder.files[i]};
 }
@@ -40,6 +38,4 @@ bool Folder::contains(const File &file) const {
 	return false;
 }
 
-void Folder::print() const {
-	for (int i=0; i<currSize; i++) files[i]->print();
-}
+void Folder::print() const { for (int i=0; i<currSize; i++) files[i]->print(); }

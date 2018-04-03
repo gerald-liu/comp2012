013a8d180a865e9eb967e1409bf5953387add8bf
gerald-liu <36258909+gerald-liu@users.noreply.github.com>
2018-04-04 00:43:47 +0800

    instructor update

--- pa2/CardPile.cpp
+++ pa2/CardPile.cpp
@@ -10,7 +10,17 @@
 CardPile& CardPile::operator +=(Card* newCard) {
 	if (newCard == nullptr)
 		return *this;
-	if (cards == nullptr || size == 0) {
+
+	// Bug, Credit @HarryWan
+	// ref: https://www.facebook.com/groups/481228118923366/permalink/599536547092522/
+	// When the CardPile resize to 0 it will not delete the array and cause memory leak.
+	//
+	// if (cards == nullptr || size == 0) {
+	//
+	// fixed as
+	//
+	// if (cards == nullptr) {
+	if (cards == nullptr) {
 		cards = new Card*[1];
 	} else {
 		Card** tmp = new Card*[1 + size];
@@ -33,6 +43,10 @@ Card* CardPile::removeCard(int index) {
 	for (int i = index; i < size - 1; i++)
 		cards[i] = cards[i + 1];
 	size--;
+	if (size == 0) {
+		delete [] cards;
+		cards = nullptr;
+	}
 	return result;
 }
 
@@ -48,7 +62,18 @@ CardPile::~CardPile() {
 
 void CardPile::shuffle()
 {
-	if (size <= 1)
+	//Bug. Credit @Yuki Lee
+	//ref: https://www.facebook.com/groups/481228118923366/permalink/601233966922780/
+	//If the pile contain only one card and that card is a WildCard or a DrawFour, the card would not be reset.
+	//Although it is possible to reset it in Player.cpp, it is indeed an unintended bug and shall be fixed as below
+	//
+	//if (size <= 1)
+	//
+	//fixed as
+	//
+	// if (size < 1)
+
+	if (size < 1)
 		return;
 
 	for (int i = 0; i < size; i++)
